2f57d4919b8eff8539b654036aebebf5ba833b9b
Nikhila Kuchimanchi <nikhilakuchimanchi@Nikhilas-MacBook-Air.local>
2026-02-24 17:42:43 -0500

    Question 2

--- hw2.py
+++ hw2.py
@@ -1,4 +1,6 @@
-#NO AI OR EXTERNAL TOOLS WERE USED FOR THIS PROBLEM
+#NO AI OR EXTERNAL TOOLS WERE USED FOR THIS HOMEWORK ASSIGNMENT
+
+# PROBLEM 1 - EXPRESSION TREE
 
 class TreeNode:
     def __init__(self, value):
@@ -51,3 +53,56 @@ if __name__ == "__main__":
     print("Inorder expression:")
     print_inorder(root)
     print()
+
+# PROBLEM 2 - TREE TRAVERSALS
+
+def prefix_traversal(root):
+    #Preorder traversal (Root, Left, Right)
+    
+    if root is None:
+        return []
+
+    return [root.value] + \
+           prefix_traversal(root.left) + \
+           prefix_traversal(root.right)
+
+
+def postfix_traversal(root):
+    #Postorder traversal (Left, Right, Root)
+
+    if root is None:
+        return []
+
+    return postfix_traversal(root.left) + \
+           postfix_traversal(root.right) + \
+           [root.value]
+
+
+def infix_traversal(root):
+    
+    #Inorder traversal (Left, Root, Right)
+    #Adds parentheses for operator nodes
+    
+    if root is None:
+        return []
+
+    
+    if root.left is None and root.right is None:
+        return [root.value]
+
+    return ['('] + \
+           infix_traversal(root.left) + \
+           [root.value] + \
+           infix_traversal(root.right) + \
+           [')']
+
+
+# Testing Example
+
+if __name__ == "__main__":
+    postfix = ["3", "4", "+", "2", "*"]
+    root = build_expression_tree(postfix)
+
+    print("Prefix:", prefix_traversal(root))
+    print("Infix:", infix_traversal(root))
+    print("Postfix:", postfix_traversal(root))
